0fbffe98f352340d99d5d4a246cb5c784e4de75f
torimusi <83056035+torimusi@users.noreply.github.com>
2022-04-18 00:28:03 +0900

    solving 3.2.3

---
+++ chapter3/3.2.3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long N;
+long long A[100009];
+
+/* 2数の最小公倍数を求める */
+long long LCM(long long a, long long b) {
+    long long A = a;
+    long long B = b;
+    
+    //2数の最大公約数を求める
+    while (a >= 1 && b >= 1) {
+        if (b > a) b = b % a;
+        else a = a % b;
+    }
+    long long GCD;
+    if (a >= 1) GCD = a;
+    else GCD = b;
+
+    //2数の積を最大公約数で割る
+    return A * B / GCD;
+}
+
+int main() {
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> A[i];
+
+    long long temp = LCM(A[1], A[2]);
+    for (int j = 3; j <= N; j++) {
+        temp = LCM(temp, A[j]);
+    }
+
+    cout << temp << endl;
+    return 0;
+}
