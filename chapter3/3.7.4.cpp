53d2f6683140dbc1cf4a13313a56f810d43ce541
torimusi <83056035+torimusi@users.noreply.github.com>
2022-04-29 21:06:38 +0900

    solving 3.7.4

---
+++ chapter3/3.7.4.cpp
@@ -0,0 +1,20 @@
+// 動的計画法を用いてO(NW)で解く
+
+#include <iostream>
+using namespace std;
+
+int N, S, A[10009];
+
+int main() {
+    cin >> N >> S;
+    for (int i = 1; i <= N; i++) cin >> A[i];
+
+    // 動的計画法
+    for (int i = 1; i <= N; i++) {
+        // 合計が S となるかどうか
+    }
+
+    printf("Yes");
+    printf("No");
+    return 0;
+}
