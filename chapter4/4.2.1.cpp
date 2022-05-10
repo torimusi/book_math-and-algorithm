b8ec9d546cc7ac0b5d25835f7fdb3ea2bd3f7032
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-10 22:09:56 +0900

    solving 4.2.1

---
+++ chapter4/4.2.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int N, M, A[10000009], B[200009];
+
+int main() {
+    cin >> N;
+    for (int i = 1; i <= N - 1; i++) cin >> A[i];
+    cin >> M;
+    for (int i = 1; i <= M; i++) cin >> B[i];
+
+    // 階差で考える
+    // 初項 と 一つ前の項との関係
+    
+    return 0;
+}
