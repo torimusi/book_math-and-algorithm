17f00e80afe526de99df9bbdcd7f88a16c4a6973
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-11 22:33:09 +0900

    solved 4.2.1

--- chapter4/4.2.1.cpp
+++ chapter4/4.2.1.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int N, M, A[10000009], B[200009];
+long long N, M, A[200009], B[200009];   // 計算にlong longが登場するならintもキャストされる
 
 int main() {
     cin >> N;
-    for (int i = 1; i <= N - 1; i++) cin >> A[i];
+    for (long long i = 1; i <= N - 1; i++) cin >> A[i];
     cin >> M;
-    for (int i = 1; i <= M; i++) cin >> B[i];
+    for (long long i = 1; i <= M; i++) cin >> B[i];
 
-    // 階差で考える
-    // 初項 と 一つ前の項との関係
+    // i番目の駅までの距離の累積和の配列
+    long long C[200009];
+    C[1] = 0;
+    for (long long i = 2; i <= N; i++) {
+        C[i] = C[i - 1] + A[i - 1];
+    }
+
+    // 移動距離の和を求める
+    long long ans = 0;
+    long long distance;
+    for (long long i = 2; i <= M; i++) {
+        distance = C[B[i]] - C[B[i - 1]];
+        if (distance < 0) distance = distance * (-1);
+        ans = ans + distance;
+    }
     
+    cout << ans << endl;
     return 0;
 }
