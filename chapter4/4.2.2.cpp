f94f4bbd19e22040085ddf43f283961c65a5cc3f
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-13 22:03:54 +0900

    solved 4.2.2

--- chapter4/4.2.2.cpp
+++ chapter4/4.2.2.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 long long T, N, L[500009], R[500009];
-long long A[500009]; // 従業員数の階差、グローバルに定義するとゼロクリア
-long long B[500009];
+// グローバルに定義するとゼロクリア、答えでは for (int i = 0; i <= T; i++) A[i] = 0; していた
+long long A[500009]; // 従業員数の階差
+long long B[500009]; // 従業員数の累積和
 
 int main() {
     cin >> T;
     cin >> N;
-    for (long long i = 1; i <= T; i++) cin >> L[i] >> R[i];
+    for (long long i = 1; i <= N; i++) cin >> L[i] >> R[i];
 
     // 従業員数の階差を求める
     for (long long i = 1; i <= N; i++) {
@@ -17,11 +18,11 @@ int main() {
     }
 
     // 従業員数の累積和を求める
-    B[0] = 0; // ゼロクリアされているはずだが一応＆わかりやすく
-    for (long long i = 1; i <= T - 1; i++) {
-        B[i] = B[i - 1] + A[i];
+    for (long long i = 0; i <= T - 1; i++) {
+        if (i == 0) B[i] = A[i];
+        else B[i] = B[i - 1] + A[i];
+        cout << B[i] << endl;
     }
 
-    for (long long i = 1; i <= T - 1; i++) cout << B[i] << endl;
     return 0;
 }
