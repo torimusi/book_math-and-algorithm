6d3503a2dcf0d8b2ed7e5935d32cbdcd70dbac31
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-17 22:09:17 +0900

    solving 4.4.3

--- chapter4/4.4.3.cpp
+++ chapter4/4.4.3.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// 素直に計算すると時間超過
+// 素数だけあらかじめ分ける？ → 間違いは無くなったが変わらず時間超過
+// ２倍は約数が一つ増えることを利用？
+
+// 素数か否かを判定
+bool prime[10000009];
+
 // 正整数の約数の個数を求める
-int divisors(int K) {
-    int sum = 0;
-    for (int i = 1; i * i <= K; i++) {
+long long divisors(long long K) {
+    long long sum = 0;
+    for (long long i = 1; i * i <= K; i++) {
         if (K % i == 0) {
             sum ++;
             if (i * i != K) sum++;
@@ -13,26 +20,25 @@ int divisors(int K) {
     return sum;
 }
 
-// 計算時間超過
-// エラトステネスのふるいの利用？
-int isDivisors(int K) {
-    bool divisors[K];
-    for (int i = 1; i <= K; i++) divisors[i - 1] = false;
-    for (int i = 1; i <= K; i ++) {
-        
-    }
-}
-
-// 素因酢分解すれば個数は簡単に求まる？
-
 int main() {
-    int N;
+    long long N;
     cin >> N;
 
+    // N以下の素数を全て出力する
+    prime[1] = false;
+    for (int i = 2; i <= N; i++) prime[i] = true;
+    for (int i = 2; i * i <= N; i ++) {
+        if (prime[i] == true) {
+            for (int j = 2 * i; j <= N; j += i) prime[j] = false;
+        }
+    }
+
     // 与関数の計算
-    int ans = 0;
-    for (int i = 1; i <= N; i++) {
-        ans += i * divisors(i);
+    long long ans = 0;
+    for (long long i = 1; i <= N; i++) {
+        // i が素数なら約数は2個
+        if (prime[i == true]) ans += i * 2;
+        else ans += i * divisors(i);
     }
 
     cout << ans << endl;
