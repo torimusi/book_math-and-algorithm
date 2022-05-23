921d365e33a04cffb2b90520a0a6f24188982ef0
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-23 21:43:12 +0900

    solved 4.5.5

--- chapter4/4.5.5.cpp
+++ chapter4/4.5.5.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N, M, A[100009], B[100009];
-vector<int> G[100009] // G[i] は頂点iに隣接する頂点のリスト
+vector<int> G[100009]; // G[i] は頂点iに隣接する頂点のリスト
 
 int main() {
     cin >> N >> M;
@@ -13,5 +13,18 @@ int main() {
         G[B[i]].push_back(A[i]); // 頂点 B[i] に隣接する頂点として A[i] を追加
     }
 
+    // 自分自身より頂点番号が小さい隣接頂点がちょうど1つ存在する頂点の数を出力する
+    int ans = 0;
+    for (int i = 1; i <= N; i++) {
+        int cnt = 0;
+        // G[i].size()は頂点 i に隣接する頂点のリストの大きさ
+        for (int j = 0; j < G[i].size(); j++) {
+            // G[i][j] は頂点 i に隣接している頂点のうち j 番目
+            if (G[i][j] < i) cnt++;
+        }
+        if (cnt == 1) ans++;
+    }
+
+    cout << ans << endl;
     return 0;
 }
