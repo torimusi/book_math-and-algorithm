bd31e7ac57415072213b9aef623657896fa24968
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-24 22:15:18 +0900

    solving 4.5.6

--- chapter4/4.5.6.cpp
+++ chapter4/4.5.6.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 using namespace std;
 
@@ -6,7 +7,8 @@ int R, C;
 int sy, sx;
 int gy, gx;
 string c[59][59];
-int restmove[59][59];
+int restmove[59][59]; // 最短手数
+int py, px; // 現在地
 
 int main() {
     cin >> R >> C;
@@ -16,11 +18,21 @@ int main() {
         for (int j = 1; j <= C; j++) cin >> c[i][j];
     }
 
+    // マスの最短手数に大きい数を入れて初期化
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) restmove[i][j] = 3000;
+    }
+
     // マスの最短手数を確定する
     restmove[sy][sx] = 0;
-
-    // 空きマスかの判定
-    if (一つ前のマス+-1 == ".") restmove[][] = 一つ前 + 1;
+    py = sy, px = sx;
+    while (py != gy || px != px) {
+        if (c[py + 1][px + 1] == ".") restmove[sy + 1][sx + 1] = min(restmove[sy + 1][sx + 1], restmove[sy][sx] + 1);
+        if (c[py + 1][px - 1] == ".") restmove[sy + 1][sx + 1] = min(restmove[sy + 1][sx + 1], restmove[sy][sx] + 1);
+        if (c[py - 1][px + 1] == ".") restmove[sy + 1][sx + 1] = min(restmove[sy + 1][sx + 1], restmove[sy][sx] + 1);
+        if (c[py - 1][px - 1] == ".") restmove[sy + 1][sx + 1] = min(restmove[sy + 1][sx + 1], restmove[sy][sx] + 1);
+        // 現在地の更新
+    }
 
     return 0;
 }
