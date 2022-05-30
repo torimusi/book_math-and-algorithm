6c4af67dcf79b53d33cfcc1ede35d67fa4060f5a
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-30 22:43:25 +0900

    solving 4.6.2

---
+++ chapter4/4.6.2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int X, Y;
+int Divider = 1000000007;
+
+int main() {
+    cin X >> Y;
+
+    // ナイトは(0, 0)からスタート
+    // 移動は(i + 1, j + 1) or (i + 2, j + 1)のみ
+    // 場合の数をまず求める
+    // 計算途中で1000000007で割ってオーバーフローを防ぐ
+
+    return 0;
+}
