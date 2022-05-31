f27b2dc34ee5e34b1483f19ffcc3edbe75bf079f
torimusi <83056035+torimusi@users.noreply.github.com>
2022-05-31 19:43:38 +0900

    solving 4.6.2

--- chapter4/4.6.2.cpp
+++ chapter4/4.6.2.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int X, Y;
-int Divider = 1000000007;
+const long long mod = 1000000007;
 
 int main() {
     cin X >> Y;
@@ -12,5 +13,14 @@ int main() {
     // 場合の数をまず求める
     // 計算途中で1000000007で割ってオーバーフローを防ぐ
 
+    long long ans;
+
+    // ナイトが移動できるか
+    if (X + Y >= 5 && (X + Y) % 3 == 2 && min(X, Y) >= (X + Y) / 3 + 1) {
+        // 移動できる場合、場合の数を求める
+        
+    } else ans = 0;
+
+    cout << ans << endl;
     return 0;
 }
